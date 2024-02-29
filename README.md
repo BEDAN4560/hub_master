feb029c045ba331a3c7c6278f786a41aa9e5be8a
Captainbedan <bedankorr@gmail.com>
2024-02-29 18:45:40 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# Test Hub 
+
+Test Hub is web application that enables students to take multiple answered questions then mark and present the score to the students.The test is timed and will end either when time is over or the student submits before time. The project uses Javascript for the logic, HTML for page structure and Vanilla css for site styling.
+***
+## Author 
+
+**Bukosia Ebenezar**
+***
+
+## Screenshots
+
+### landing page
+#### Mobile
+![image](/assets/images/landingPageM.png)
+#### Desktop
+![image](/assets/images/landingPage.png)
+***
+### test page
+#### Mobile
+![image](/assets/images/testPageM.png)
+#### Desktop
+![image](/assets/images/testPageD.png)
+***
+### results page
+#### Mobile
+![image](./assets/images/resultsPageM.png)
+#### Desktop
+![image](/assets/images/resultspage.png)
+***
+## Figma Designs
+- landing page
+  https://www.figma.com/file/bLyP3eHm0qrAxW8Rs3hzBg/landingpagePC?node-id=2%3A2
+- test page
+  https://www.figma.com/file/q7fcFtHIOATLWMITqM46n7/testpagePC?node-id=0%3A1  
+
+---
+## Table of content
+- [Technologies](#description)
+- [Description](#description)
+- [Features](#features)
+- [Setup-process](#setup_process)
+- [Project-usage](#project-usage)
+- [Copyright](#copyright)
+- [Licence](#licence)
+  
+---
+## Technologies
+
+languages used are: 
+- HTML :HTML5 -which is used to create the structure of thr page
+- CSS :CSS4 -used to style the page
+- JavaScript -used to code app logic
+
+---
+### Features
+As a user you ase able to:
+  1 navigate through the site
+  2 user can take a test
+  3 app will mark and grade the students
+  4 the app will time the student 
+  5 the app will display students results after submission or when time is over
+
+---
+### description
+I used java script to create a dynamic site and also to create some complex function such as timing and marking and grading of the test.
+I used html in creating the page structure and css in styling the document.
+I also used an external css to enable me to use fa icons
+css link https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css
+
+--- 
+### challenges 
+The major challenge I faces was implementing timer per question function. 
+
+---
+## How to set up and run the project
+
+#### method one
+clone the repo using the command
+- $git clone https://github.com/Ebenezr/Test-hub.git
+change directory using command
+- $cd landing-page
+
+---
+#### live link
+   \- click the following link to view the application https://ebenezr.github.io/Test-hub/
+
+***
+## How to use the project
+
+The project root folder contains two files, main **index.html** and **README.md** file and one folder assets folder. the assets folder within it are four folders **css** which contains css files and **images** folder which contains images *js* which contains JavaScript files and pages folder containing secondary pages used in the project.
+
+***
+## Copyright
+ Copyright(c)[2022][Bukosia Ebenezar]
+
+***
+## Licence
+
+MIT License
+
+***"# hub_master" 
+"# hub_master" 
---
+++ assets/js/main.js
@@ -0,0 +1,182 @@
+// Questions and answers data bank
+
+const questionsObject = [
+  {
+      question: "How do you print something in javascript?",
+      a: "console.log('print)",
+      b: "print('print')",
+      c: "printf('print')",
+      d: "log('print')",
+      correct: "a",
+  },
+  {
+      question: "What is the other name of javascript?",
+      a: "ECMAscript",
+      b: "JS",
+      c: "none of the above",
+      d: "all of the above",
+      correct: "b",
+  },
+  {
+      question: "Is java an oop language?",
+      a: "no",
+      b: "not entirely true",
+      c: "yes",
+      d: "none of the above",
+      correct: "c",
+  },
+  {
+      question: "Where can you add external javascript?",
+      a: "inside head tag",
+      b: "inside body tag",
+      c: "all of the above",
+      d: "none of the above",
+      correct: "c",
+  },
+  {
+      question: "How do you call a javascript method?",
+      a: "method()",
+      b: "call method()",
+      c: "run method()",
+      d: "none of the above",
+      correct: "a",
+},
+
+
+];
+
+
+const quiz= document.getElementById('quiz-wrapper')
+//get radio buttons values from html
+const radioOptions = document.querySelectorAll('.options')
+//gets and prints the questions in html 
+const questionWrite = document.getElementById('question')
+//prints answer options to the html radio labels || four options are used 
+//gets radio buttons values from html
+const optionA = document.getElementById('optionA')
+const optionB = document.getElementById('optionB')
+const optionC = document.getElementById('optionC')
+const optionD = document.getElementById('optionD')
+//link the submit values from html
+const submitBtn = document.getElementById('submit')
+
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz()
+
+//displays first question
+function loadQuiz() {
+
+  deselectAnswers()
+
+  const currentquestionsObject = questionsObject[currentQuiz]
+
+  questionWrite.innerText = currentquestionsObject.question
+  optionA.innerText = currentquestionsObject.a
+  optionB.innerText = currentquestionsObject.b
+  optionC.innerText = currentquestionsObject.c
+  optionD.innerText = currentquestionsObject.d
+}
+
+// timer function timer set to 60 seconds initial
+var sec = 60;
+var time = setInterval(myTimer, 1000);
+
+function myTimer() {
+    document.getElementById('timer').innerHTML = sec + " sec";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        alert("Time out!! :(");
+        //if time is up finish test an print results
+        quiz.innerHTML = `
+         <h2>You Scored <em>${calGrade(score)}</em> of ${score*20} % </h2>
+          
+         <button onclick="location.reload()">ReTake Test</button>
+         `
+    }
+}
+//function to reset local db
+function deselectAnswers() {
+  radioOptions.forEach(answerEl => answerEl.checked = false)
+}
+
+
+//function to check which option has been selected
+function getSelected() {
+  //initialize variable to store selected option
+  let answer
+  //forEach loop to loop through all radio buttons and get the selected value
+  radioOptions.forEach(answerEl => {
+      if(answerEl.checked) {
+          answer = answerEl.id
+      }
+  })
+  return answer
+}
+
+//function to listen to button events and store scores locally
+submitBtn.addEventListener('click', () => {
+  const answer = getSelected()
+  if(answer) {
+     if(answer === questionsObject[currentQuiz].correct) {
+         score++
+     }
+
+     currentQuiz++
+//if test is done execute...
+     if(currentQuiz < questionsObject.length) {
+         loadQuiz()
+     } else {
+       //computes score percentage inside I call function to compute grades with score as its argument
+       // prints results grade and score in percentage
+       quiz.innerHTML = `
+         <h2>You Scored <em>${calGrade(score)}</em> of ${score*20} % </h2>
+          
+         <button onclick="location.reload()">Reload</button>
+         `
+     }
+  }
+})
+
+//function to compute score grade
+function calGrade(score){
+  //i multiplied by 20 to make it out of 100 (%) 
+  var tempVal=score*20;
+  var grade;
+      
+  if (tempVal <= 100 && tempVal >= 80){
+        grade ='A'
+  }
+  
+  else if(tempVal <= 89 && tempVal >= 70){
+        grade ='B'
+  }
+  
+  else if(tempVal <= 69 && tempVal >= 50){
+        grade ='C'
+  }
+  
+  else if(tempVal <= 49 && tempVal >= 40){
+        grade ='D'
+  }
+  
+  else if(tempVal < 39 ){
+        grade ='F'
+  }
+  return grade;
+}
+
+//alert box
+function myAlert() {
+  alert("Oops Site still under construction :(");
+}
+//prompt box
+function myConfirm() {  
+  if(confirm ("You will be redirected to the test page and timer started"))
+  {
+    window.location.href="./assets/pages/test.html";
+  }
+  } 
